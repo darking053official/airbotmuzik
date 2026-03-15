ebc90301ad4c96f708bb4829d382e44e26f05381
Darking053 <darking053@protonmail.com>
2026-03-15 19:26:53 +0300

    Update pause.js

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -1,29 +1,17 @@
-const config = require('../config');
+const config = require('../../config');
 
 module.exports = {
   name: 'pause',
-  description: 'Çalan müziği duraklatır',
+  description: 'Müziği duraklatır',
   aliases: ['durdur', 'bekle'],
   cooldown: 3,
   
   async execute(message, args, client) {
     const voiceChannel = message.member?.voice?.channel;
-    if (!voiceChannel) {
-      return message.reply(config.messages.notInVoiceChannel);
-    }
-
-    // DJ rolü kontrolü
-    const djRoleId = client.djRoles?.get(message.guild.id);
-    if (config.djRole.enabled && config.djRole.requiredFor.includes('pause') && djRoleId) {
-      if (!message.member.roles.cache.has(djRoleId) && !message.member.permissions.has('ADMINISTRATOR')) {
-        return message.reply(config.messages.needDjRole);
-      }
-    }
+    if (!voiceChannel) return message.reply(config.messages.notInVoiceChannel);
 
     const serverQueue = client.queue?.get(message.guild.id);
-    if (!serverQueue || serverQueue.songs.length === 0) {
-      return message.reply(config.messages.noMusicPlaying);
-    }
+    if (!serverQueue) return message.reply(config.messages.noMusicPlaying);
 
     serverQueue.player.pause();
     return message.reply(config.messages.paused);
