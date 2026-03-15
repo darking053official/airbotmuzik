46b1f4ef4e746c904b486b23725515e0f90c060b
Darking053 <darking053@protonmail.com>
2026-03-15 19:11:47 +0300

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,35 +3,19 @@ const config = require('../config');
 module.exports = {
   name: 'stop',
   description: 'Müziği durdurur ve kanaldan çıkar',
-  aliases: ['dur', 'leave', 'git', 'ayril'],
+  aliases: ['dur', 'leave', 'git'],
   cooldown: 3,
   
   async execute(message, args, client) {
     const voiceChannel = message.member?.voice?.channel;
-    if (!voiceChannel) {
-      return message.reply(config.messages.notInVoiceChannel);
-    }
-
-    // DJ rolü kontrolü
-    const djRoleId = client.djRoles?.get(message.guild.id);
-    if (config.djRole.enabled && config.djRole.requiredFor.includes('stop') && djRoleId) {
-      if (!message.member.roles.cache.has(djRoleId) && !message.member.permissions.has('ADMINISTRATOR')) {
-        return message.reply(config.messages.needDjRole);
-      }
-    }
+    if (!voiceChannel) return message.reply(config.messages.notInVoiceChannel);
 
     const serverQueue = client.queue?.get(message.guild.id);
-    if (!serverQueue) {
-      return message.reply(config.messages.noMusicPlaying);
-    }
+    if (!serverQueue) return message.reply(config.messages.noMusicPlaying);
 
     serverQueue.songs = [];
     serverQueue.player.stop();
-    
-    if (serverQueue.connection && config.voice.leaveOnStop) {
-      serverQueue.connection.destroy();
-    }
-    
+    if (serverQueue.connection) serverQueue.connection.destroy();
     client.queue.delete(message.guild.id);
     
     return message.reply(config.messages.stopped);
