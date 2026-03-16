274856a7017deb82b26ea8a882424f9c814b31db
Darking053 <darking053@protonmail.com>
2026-03-16 11:55:35 +0300

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,23 +1,18 @@
-const config = require('../../config');
-
 module.exports = {
   name: 'stop',
-  description: 'Müziği durdurur ve kanaldan çıkar',
-  aliases: ['dur', 'leave', 'git'],
-  cooldown: 3,
+  description: 'Müziği durdurur',
+  aliases: ['dur'],
   
   async execute(message, args, client) {
-    const voiceChannel = message.member?.voice?.channel;
-    if (!voiceChannel) return message.reply(config.messages.notInVoiceChannel);
-
-    const serverQueue = client.queue?.get(message.guild.id);
-    if (!serverQueue) return message.reply(config.messages.noMusicPlaying);
+    const serverQueue = client.queue.get(message.guild.id);
+    
+    if (!serverQueue || !serverQueue.songs.length) {
+      return message.reply('❌ **Çalan müzik yok!**');
+    }
 
     serverQueue.songs = [];
-    serverQueue.player.stop();
-    if (serverQueue.connection) serverQueue.connection.destroy();
-    client.queue.delete(message.guild.id);
+    serverQueue.player?.stop();
     
-    return message.reply(config.messages.stopped);
+    await message.reply('⏹️ **Müzik durduruldu!**');
   }
 };
